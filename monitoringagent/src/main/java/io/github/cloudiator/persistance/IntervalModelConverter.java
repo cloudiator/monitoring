7022c8d2b706e4e9a6cb5c5ef7370ea9a602b5cb
Daniel Baur <daniel.baur@uni-ulm.de>
2020-01-10 14:04:30 +0100

    fixed timeunit

--- monitoringagent/src/main/java/io/github/cloudiator/persistance/IntervalModelConverter.java
+++ monitoringagent/src/main/java/io/github/cloudiator/persistance/IntervalModelConverter.java
@@ -2,7 +2,7 @@ package io.github.cloudiator.persistance;
 
 import de.uniulm.omi.cloudiator.util.OneWayConverter;
 import io.github.cloudiator.rest.model.Interval;
-import io.github.cloudiator.rest.model.Interval.UnitEnum;
+import io.github.cloudiator.rest.model.TimeUnit;
 import javax.annotation.Nullable;
 
 public class IntervalModelConverter implements OneWayConverter<IntervalModel, Interval> {
@@ -15,7 +15,7 @@ public class IntervalModelConverter implements OneWayConverter<IntervalModel, In
       return null;
     }
     Interval interval = new Interval()
-        .unit(UnitEnum.valueOf(intervalModel.getUnit().name()))
+        .unit(TimeUnit.valueOf(intervalModel.getUnit().name()))
         .period(intervalModel.getPeriod());
     return interval;
   }
